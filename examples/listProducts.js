e57c555437401823dc31c485f8fd5a7d810ab073
Autarc <Autarc@gmail.com>
2015-01-15 15:11:28 +0100

    implement streaming interface

--- examples/listProducts.js
+++ examples/listProducts.js
@@ -2,16 +2,29 @@
  * List Product
  * ============
  *
- * Create a list of all product ids for games and dlcs available in germany.
+ * Create a list of all mods available in germany, using the streaming interface
+ * to handle each product as it got received.
+ *
+ * A promisified handler can be invoke by setting neglecting the second argument.
+ *
+ * store.getProducts('mod').then(function (products) {
+ *
+ * });
  */
 
 var SteamStore = require('../index');
+var JSONStream = require('JSONStream');
 
 var store = new SteamStore({
   country: 'DE',
   language: 'de'
 });
 
-store.getProducts(['game', 'dlc']).then(function (products) {
-  console.log(products);
-});
+store.getProducts('mod', true)
+     .pipe(JSONStream.parse())
+     .on('data', function (data) {
+        console.log(data.name);
+      })
+     .on('end', function(){
+      console.log('END')
+     });
